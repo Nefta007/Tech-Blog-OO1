1aa476835518a1d5d5d73837d63c55a822ae6f54
Nefta007 <nperc001@ucr.edu>
2023-12-07 14:37:55 -0800

    added sess, port, app and hbs to server.js

--- server.js
+++ server.js
@@ -1,3 +1,27 @@
+const path = require('path');
 const express = require('express');
 const expressHandle = require('express-handlebars');
 const session  = require('express-session');
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({ helpers });
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {
+      maxAge: 300000,
+      httpOnly: true,
+      secure: false,
+      sameSite: 'strict',
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  };
