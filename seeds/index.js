d67b425fc777ffe91bae33bb6826b03510cf797d
Nefta007 <nperc001@ucr.edu>
2023-12-12 03:38:11 -0800

    changed format for index.js in seed

--- seeds/index.js
+++ seeds/index.js
@@ -1,8 +1,8 @@
+const sequelize = require('../config/connection');
 const seedPost = require('./blogPost-seeds');
 const seedUser = require('./blogUser-seeds');
 //const seedComment = require('./product-tag-seeds');
 
-const sequelize = require('../config/connection');
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
