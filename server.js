479cde0111b1fdcd0c38e7382cae62fcde065000
Nefta007 <nperc001@ucr.edu>
2023-12-12 04:09:54 -0800

    added to main handlebar

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const routes = require('./controllers');
 const express = require('express');
 const exphbs = require('express-handlebars');
-const session  = require('express-session');
+const session = require('express-session');
 const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -14,20 +14,20 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-    secret: 'Super secret secret',
-    cookie: {
-      maxAge: 3000000,
-      // httpOnly: true,
-      // secure: false,
-      // sameSite: 'strict',
-    },
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-      db: sequelize
-    })
-  };
-  app.use(session(sess));
+  secret: 'Super secret secret',
+  cookie: {
+    maxAge: 3000000,
+    // httpOnly: true,
+    // secure: false,
+    // sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+app.use(session(sess));
 
 // Inform Express.js on which template engine to use
 app.engine('handlebars', hbs.engine);
